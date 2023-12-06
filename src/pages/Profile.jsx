56594bd39c1aa67db80c0e6f0137824b9812b543
Jason Phillips <jasonanthonyp@gmail.com>
2023-12-05 22:32:43 -0500

    create routes

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Profile() {
+    return (
+        <div>
+            <div>
+                {/* profile picture */}
+                <div>
+                    <h2>Name</h2>
+                    <h3>Description</h3>
+                </div>
+            </div>
+            <div>
+                {/* start project button */}
+                {/* previous projects */}
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default Profile
