6c408d7bc325e87bb6ac41af02033f71a8d1fc8e
Jason Phillips <jasonanthonyp@gmail.com>
2023-12-19 12:30:00 -0500

    commit all changes to api route

--- api/routes/casting.route.js
+++ api/routes/casting.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCasting, deleteCasting, updateCasting, getCasting } from '../controllers/casting.controller.js';
+import { createCasting, deleteCasting, updateCasting, getCasting, getCastings } from '../controllers/casting.controller.js';
 import { verifyToken } from '../utils/verifyUser.js'
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.post('/create', verifyToken, createCasting);
 router.delete('/delete/:id', verifyToken, deleteCasting);
 router.post('/update/:id', verifyToken, updateCasting);
 router.get('/get/:id', getCasting);
+router.get('/get', getCastings);
 
 
 export default router;
